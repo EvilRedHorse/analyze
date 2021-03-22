0300d59795a492c64ceb891c98e3da2539df6f18
Matthew Sevey <mjsevey@gmail.com>
2021-03-22 11:09:16 -0600

    Add documentation in lockcheck

--- lockcheck/lockcheck.go
+++ lockcheck/lockcheck.go
@@ -24,6 +24,7 @@ var Analyzer = &analysis.Analyzer{
 	},
 }
 
+// run implements the analysis interface
 func run(pass *analysis.Pass) (interface{}, error) {
 	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 
@@ -51,6 +52,10 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	return nil, nil
 }
 
+// isSyncObject is a helper to determing if the object is a sync package object
+//
+// We check for golang's sync packages as well as NebulousLabs TryMutex and
+// ThreadGroup packages
 func isSyncObject(t types.Type) bool {
 	switch t.String() {
 	case "sync.Mutex",
@@ -63,14 +68,21 @@ func isSyncObject(t types.Type) bool {
 	return false
 }
 
+// isMutexType is a helper for determining if the object type is a mutex
 func isMutexType(t types.Type) bool {
+	// The type is a mutex type if it has a `Mutex` suffix
 	return strings.HasSuffix(t.String(), "Mutex")
 }
 
+// containsMutex is a helper that checks if an object contains a mutex
 func containsMutex(pass *analysis.Pass, recv types.Object) (types.Object, bool) {
+	// Grab the underlying Type Object??
 	if p, ok := recv.Type().Underlying().(*types.Pointer); ok {
+		// Crab the struct of the pointer's underlying element
 		if s, ok := p.Elem().Underlying().(*types.Struct); ok {
+			// Iterate over the struct's fields
 			for i := 0; i < s.NumFields(); i++ {
+				// Check if the field is a Mutex Type and the name is `mu`
 				if f := s.Field(i); isMutexType(f.Type()) && s.Field(i).Name() == "mu" {
 					return s.Field(i), true
 				}
@@ -80,29 +92,21 @@ func containsMutex(pass *analysis.Pass, recv types.Object) (types.Object, bool) 
 	return nil, false
 }
 
+// checkLockSafety is the main logic function for lockcheck
 func checkLockSafety(pass *analysis.Pass, fd *ast.FuncDecl, recv, recvMu types.Object) {
 	name := fd.Name.String()
 	recvIsPrivileged := managesOwnLocking(name)
 
 	cfgs := pass.ResultOf[ctrlflow.Analyzer].(*ctrlflow.CFGs)
 
+	// isLock is a helper that checks for mu.Lock()
 	isLock := func(block ast.Node) bool {
 		var found bool
 		ast.Inspect(block, func(n ast.Node) bool {
 			if found {
 				return false
 			}
-			if ce, ok := n.(*ast.CallExpr); ok {
-				if fnse, ok := ce.Fun.(*ast.SelectorExpr); ok {
-					if strings.HasSuffix(fnse.Sel.Name, "Lock") {
-						if se, ok := fnse.X.(*ast.SelectorExpr); ok {
-							if sel, ok := pass.TypesInfo.Selections[se]; ok && sel.Obj() == recvMu {
-								found = true
-							}
-						}
-					}
-				}
-			}
+			found = isMutexCall(pass, recvMu, n, "Lock")
 			// don't descend into DeferStmt or FuncLit
 			if _, ok := n.(*ast.DeferStmt); ok {
 				return false
@@ -113,6 +117,7 @@ func checkLockSafety(pass *analysis.Pass, fd *ast.FuncDecl, recv, recvMu types.O
 		})
 		return found
 	}
+	// isUnlock is a helper that checks for mu.Unlock()
 	isUnlock := func(block ast.Node) bool {
 		var found bool
 		ast.Inspect(block, func(n ast.Node) bool {
@@ -125,21 +130,12 @@ func checkLockSafety(pass *analysis.Pass, fd *ast.FuncDecl, recv, recvMu types.O
 			} else if _, ok := n.(*ast.FuncLit); ok {
 				return false
 			}
-			if ce, ok := n.(*ast.CallExpr); ok {
-				if fnse, ok := ce.Fun.(*ast.SelectorExpr); ok {
-					if strings.HasSuffix(fnse.Sel.Name, "Unlock") {
-						if se, ok := fnse.X.(*ast.SelectorExpr); ok {
-							if sel, ok := pass.TypesInfo.Selections[se]; ok && sel.Obj() == recvMu {
-								found = true
-							}
-						}
-					}
-				}
-			}
+			found = isMutexCall(pass, recvMu, n, "Unlock")
 			return true
 		})
 		return found
 	}
+	// isFieldAccess is a helper that checks for struct field accesses
 	isFieldAccess := func(block ast.Node) (field *ast.Ident, ok bool) {
 		ast.Inspect(block, func(n ast.Node) bool {
 			if field != nil {
@@ -164,6 +160,8 @@ func checkLockSafety(pass *analysis.Pass, fd *ast.FuncDecl, recv, recvMu types.O
 		})
 		return field, field != nil
 	}
+	// isRecvMethodCall is a helper that checks for a method call on
+	// a struct/object
 	isRecvMethodCall := func(block ast.Node) (method string, ok bool) {
 		ast.Inspect(block, func(n ast.Node) bool {
 			if method != "" {
@@ -184,7 +182,7 @@ func checkLockSafety(pass *analysis.Pass, fd *ast.FuncDecl, recv, recvMu types.O
 		})
 		return method, method != ""
 	}
-
+	// isFuncLitCall is a helper that checks for a function literal call
 	isFuncLitCall := func(block ast.Node) (litBlock *cfg.Block, ok bool) {
 		ast.Inspect(block, func(n ast.Node) bool {
 			if litBlock != nil {
@@ -218,30 +216,51 @@ func checkLockSafety(pass *analysis.Pass, fd *ast.FuncDecl, recv, recvMu types.O
 	}
 	visited := make(map[edge]struct{})
 	var checkPath func(*cfg.Block, bool)
+
+	// checkPath is a helper for checking a path for a function
 	checkPath = func(b *cfg.Block, lockHeld bool) {
 		for _, n := range b.Nodes {
+			// Check paths of function literal calls
 			if litBlock, ok := isFuncLitCall(n); ok {
 				checkPath(litBlock, lockHeld)
 				continue
 			}
 			if isLock(n) {
+				// mu.Lock call found
 				if !recvIsPrivileged {
 					pass.Reportf(n.Pos(), "unprivileged method %s locks mutex", name)
 				}
 				lockHeld = true
 			} else if isUnlock(n) {
+				// mu.Unlock call found
 				lockHeld = false
 			} else if method, ok := isRecvMethodCall(n); ok && !firstWordIs(method, "static") {
+				// Method call found that is not a static method
 				if recvIsPrivileged {
+					// The original object is a managed method
+					//
+					// First check for calling another managed method while holding
+					// a lock.  Ignore threaded methods as those should be called in a go
+					// routine and therefore should not create a dead lock
+					//
+					// TODO: probably should try and add check for calling threaded
+					// methods without go routine
+					//
+					// Second check if we calling an unmanaged method without the lock held
 					if managesOwnLocking(method) && !firstWordIs(method, "threaded") && lockHeld {
 						pass.Reportf(n.Pos(), "privileged method %s calls privileged method %s while holding mutex", name, method)
 					} else if !managesOwnLocking(method) && !lockHeld {
 						pass.Reportf(n.Pos(), "privileged method %s calls unprivileged method %s without holding mutex", name, method)
 					}
 				} else if managesOwnLocking(method) {
+					// The original object is not a managed method, so we should not be
+					// calling a managed method.
 					pass.Reportf(n.Pos(), "unprivileged method %s calls privileged method %s", name, method)
 				}
 			} else if field, ok := isFieldAccess(n); ok && !firstWordIs(field.Name, "static") && !firstWordIs(field.Name, "atomic") && !lockHeld {
+				// Struct field access found that is not static of atomic while no lock
+				// is being held
+				//
 				// NOTE: a method call is also considered a field access, so
 				// it's important that we only examine field accesses that
 				// aren't method calls (on recv).
@@ -264,6 +283,13 @@ func checkLockSafety(pass *analysis.Pass, fd *ast.FuncDecl, recv, recvMu types.O
 }
 
 // managesOwnLocking returns whether a method manages its own locking.
+//
+// atomic methods use sync.Atomic to be thread safe and don't require a mutex
+// extern methods are handled by a mutex external to the struct's primary mutex
+// managed, call, and threaded all handle the structs mutex
+//   - managed is a synchronous method within a subsystem
+//   - call is a synchronous method used by other subsystems
+//   - threaded is an asynchronous method
 func managesOwnLocking(name string) bool {
 	return isManagedExported(name) ||
 		firstWordIs(name, "atomic") ||
@@ -288,4 +314,20 @@ func firstWordIs(name, prefix string) bool {
 	}
 	suffix := strings.TrimPrefix(name, prefix)
 	return len(suffix) > 0 && ast.IsExported(suffix)
+}
+
+// isMutexCall is a helper that checks for a mutex call
+func isMutexCall(pass *analysis.Pass, recvMu types.Object, n ast.Node, muStr string) bool {
+	if ce, ok := n.(*ast.CallExpr); ok {
+		if fnse, ok := ce.Fun.(*ast.SelectorExpr); ok {
+			if strings.HasSuffix(fnse.Sel.Name, muStr) {
+				if se, ok := fnse.X.(*ast.SelectorExpr); ok {
+					if sel, ok := pass.TypesInfo.Selections[se]; ok && sel.Obj() == recvMu {
+						return true
+					}
+				}
+			}
+		}
+	}
+	return false
 }
